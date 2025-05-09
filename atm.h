c7b6426d4d1334376060aa948f5a0f572a51f625
Karlanicol06 <karla.gutierrez1701@alumnos.udg.mx>
2025-05-09 08:15:12 -0600

    Update atm.h

--- atm.h
+++ atm.h
@@ -1,117 +1,95 @@
-#ifndef ATM_H_INCLUDED
-#define ATM_H_INCLUDED
-
+#ifndef PRACTICAJA_H
+#define PRACTICAJA_H
 
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
-#define MAX_INTENTOS 3
-
-struct clientes{
-    int noCliente;
+struct cliente {
+    int Ncliente;
     float saldo;
     char nombre[40];
-    int pin;
+    int nip;
 };
 
-
-int validarPIN(int pinIngresado, int pin) {
-        return(pinIngresado == pin);
+int validar(float x) {
+    if (x > 0) {
+        printf("La cantidad ingresada es válida.\n");
+        return 1;
+    } else {
+        printf("La cantidad ingresada es negativa, ingrese un número positivo\n");
+        return 0;
     }
-
-void consultarSaldo(float saldo)
-{
-        printf("Tu saldo es: $%.2f\n", saldo);
 }
 
-float depositar(float saldo, float cantidad)
-{
-        if (cantidad > 0)
-        {
-                saldo += cantidad;
-                printf("Deposito exitoso. Nuevo saldo: $%.2f\n", saldo);
-        }
-        else
-        {
-                printf("Cantidad invalida.\n");
-        }
-        return saldo;
+void consultarSaldo(struct cliente clientes[], int cuentaSeleccionada) {
+    printf("========= CONSULTA DE SALDO ==========\n");
+    printf("Cliente: %s\n", clientes[cuentaSeleccionada].nombre);
+    printf("Su saldo actual es de: %.2f\n", clientes[cuentaSeleccionada].saldo);
 }
 
-float retirar(float saldo, float cantidad)
-{
-        if (cantidad > 0 && cantidad <= saldo)
-        {
-                saldo -= cantidad;
-                printf("Retiro exitoso. Nuevo saldo: $%.2f\n", saldo);
-        }
-        else
-        {
-                printf("Cantidad invalida o saldo insuficiente.\n");
-        }
-        return saldo;
+void realizarDeposito(struct cliente clientes[], int cuentaSeleccionada) {
+    float num;
+    int v;
+    printf("========= DEPOSITOS ==========\n");
+    printf("Ingrese la cantidad que desea depositar: \n");
+    scanf("%f", &num);
+    v = validar(num);
+    while (v != 1) {
+        scanf("%f", &num);
+        v = validar(num);
+    }
+    clientes[cuentaSeleccionada].saldo += num;
+    printf("Su depósito se realizó con éxito. El saldo actual es: %.2f\n", clientes[cuentaSeleccionada].saldo);
 }
 
-void menu(int cuenta, struct clientes cliente[])
-{
-        int pinIngresado;
-        int intentos = 0;
-        float cantidadDeposito,cantidadRetiro;
-        int opcion;
-
-        while (intentos < MAX_INTENTOS)
-        {
-                printf("Introduce tu PIN: ");
-                scanf("%d", &pinIngresado);
-
-                if (validarPIN(pinIngresado, cliente[cuenta].pin))
-                {
-                    printf("Bienvenido %s\n",cliente[cuenta].nombre);
-                        do
-                        {
-                                printf("\n--- Cajero Automatico ---\n");
-                                printf("1. Consultar saldo\n");
-                                printf("2. Depositar\n");
-                                printf("3. Retirar\n");
-                                printf("4. Salir\n");
-                                printf("Elige una opcion: ");
-                                scanf("%d", &opcion);
-
-                                switch (opcion)
-                                {
-                                case 1:
-                                        consultarSaldo(cliente[cuenta].saldo);
-                                        break;
-                                case 2:
-                                        printf("Introduce la cantidad a depositar: ");
-                                        scanf("%f", &cantidadDeposito);
-                                        cliente[cuenta].saldo = depositar(cliente[cuenta].saldo, cantidadDeposito);
-                                        break;
-                                case 3:
-                                        printf("Introduce la cantidad a retirar: ");
-                                        scanf("%f", &cantidadRetiro);
-                                        cliente[cuenta].saldo = retirar(cliente[cuenta].saldo, cantidadRetiro);
-                                        break;
-                                case 4:
-                                        printf("Gracias por usar el cajero automatico.\n");
-                                        break;
-                                default:
-                                        printf("Opcion invalida.\n");
-                                }
-                        } while (opcion != 4);
-                        exit(0); // Salir del programa después de un acceso exitoso
-                }
-                else
-                {
-                        printf("PIN incorrecto\n");
-                        intentos++;
-                }
+void realizarRetiro(struct cliente clientes[], int cuentaSeleccionada) {
+    float num;
+    int v;
+    do {
+        printf("========= RETIROS ==========\n");
+        printf("Ingrese la cantidad que desea retirar: \n");
+        scanf("%f", &num);
+        v = validar(num);
+        while (v != 1) {
+            scanf("%f", &num);
+            v = validar(num);
         }
 
-        // Si se exceden los intentos, bloquear el acceso y cerrar el programa
-        printf("Acceso bloqueado");
-        exit(0);
+        if (num > clientes[cuentaSeleccionada].saldo) {
+            printf("No tienes suficiente saldo, por favor ingresa una cantidad válida:\n");
+        } else {
+            clientes[cuentaSeleccionada].saldo -= num;
+            printf("Su retiro se realizó con éxito. Saldo restante: %.2f\n", clientes[cuentaSeleccionada].saldo);
+            break;
+        }
+    } while (1);
 }
 
-#endif // ATM_H_INCLUDED
+int autenticarNip(struct cliente clientes[], int cuentaSeleccionada) {
+    int nipIngresado;
+    int intentos = 0;
+    int maxIntentos = 3;
+
+    while (intentos < maxIntentos) {
+        printf("\n***** INGRESE SU NIP *****\n");
+        scanf("%d", &nipIngresado);
+
+        if (nipIngresado == clientes[cuentaSeleccionada].nip) {
+            printf("NIP ingresado correctamente.\n");
+            return 1;
+        } else {
+            intentos++;
+            printf("El NIP ingresado es incorrecto.\n");
+        }
+
+        if (intentos == maxIntentos) {
+            printf("Has alcanzado el número máximo de intentos.\n");
+            return 0;
+        }
+    }
+    return 0;
+}
+
+#endif
+
 
